5f0be5e3a6ea58a1bd37e54dfeefcfc4e5339ab2
Nitrosito <nitrosito@gmail.com>
2015-10-27 17:27:22 +0100

    Añadido archivo ismael de prueba

---
+++ prueba.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+#include "fecha.h"
+
+using namespace std;
+
+
+
+int main(){
+	/*string s = "11/03/1988";
+	cout << "Imprimo todo el string: " << s << endl;
+
+	int ini = 0;
+	int fin = s.find_first_of("/");
+
+	cout << "find_first_of() devuelve un entero a la posición dond encuentre primero lo que pongas entre parentesis: " << s.find_first_of("/") << endl;
+
+	string aux = s.substr(ini,2);
+	int i_dec = stoi(aux);
+	cout << "mes: " << i_dec << endl;
+	cout << "mes: " << aux << endl;
+
+	ini = fin;
+	fin = s.find_first_of("/",ini+1);	//hay que buscar desde la posicion siguiente a dond esta el primer "/"
+
+	aux = s.substr(3,2);
+	cout << "dia: " << aux << endl;
+
+
+	cout << "Ini: " << ini << ", Fin: " << fin << endl;
+	ini = fin;
+	fin = s.size();
+
+	aux = s.substr(6,4);
+	cout << "año: " << aux << endl;
+
+	//cout << s.find_first_of(",") << endl;*/
+	fecha una_fecha();
+	cout << una_fecha << endl;
+} 
