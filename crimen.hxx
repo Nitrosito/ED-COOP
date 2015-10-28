89cf8612915726391191f77570251f47707d7c11
Nitrosito <nitrosito@gmail.com>
2015-10-28 18:13:12 +0100

    Añadidos 5 metodos de @nitrosito

--- crimen.hxx
+++ crimen.hxx
@@ -1,14 +1,44 @@
 #include <iostream>
 using namespace std;
-  crimen();
+  crimen::crimen(){
+	ID=0;
+	casenumber="0";
+	date=("00/00/0000 00:00:00 AM");	
+	iucr="0000";
+	primarytype="0";
+	description="0";
+	location_descrip="0";
+	arrest=false;
+	domestic=false;
+	latitude=0;
+	longitude=0;
+  } 
 
-  crimen(const crimen& x);
+  crimen(const crimen& x){
+	ID=x.ID;
+	casenumber=x.casenumber;
+	date=x.date;	
+	iucr=x.iucr;
+	primarytype=x.primarytype;
+	description=x.description;
+	location_descrip=x.location_descrip;
+	arrest=x.arrest;
+	domestic=x.domestic;
+	latitude=x.latitude;
+	longitude=x.longitude;
+  }
 
-  void setID(long int & id);
+  void setID(long int & id){
+	this.ID=id;
+  }
 
-  void setCaseNumber(const string & s);
+  void setCaseNumber(const string & s){
+	this.casenumber=s;
+  }
 
-  void setDate(const fecha & d);
+  void setDate(const fecha & d){
+	this.date=d;
+  }
 
   void setIucr(const string & s);
 
@@ -52,22 +82,6 @@ using namespace std;
    bool operator==(const crimen & x) const;
 
    bool operator<(const crimen & x) const;
-
- private:
-   friend ostream& operator<< ( ostream& , const crimen& );
   
-   //Atributos 
-   long int ID;             // Identificador del delito
-   string casenumber;       // Código del caso
-   fecha date;              // Fecha en formato mm/dd/aaaa hh:mm:ss AM/PM
-   string iucr;             // Código del tipo de delito según Illinois Uniform Crime Reporting, IUCR
-   string primarytype;      // Tipo de delito
-   string description       // Descripción más detallada
-   string location_descrip  // Descripción del tipo de localización
-   bool arrest              // Si hay arrestos o no
-   bool domestic            // Si es un crimen domestico o no
-   double latitude          // Coordenada de latitud
-   double longitude         // Coordenada de longitud
-};
-
-  ostream& operator<< ( ostream& , const crimen& );
+    ostream& operator<< ( ostream& , const crimen& );
+  
