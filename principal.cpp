31505ae018e63e568c6dcfd2413c2cc24acc0ed5
Nitrosito <nitrosito@gmail.com>
2015-11-17 23:05:54 +0100

    Añadido y probado abegin() y aend()

--- principal.cpp
+++ principal.cpp
@@ -5,7 +5,7 @@
 #include <fstream>
 
 using namespace std;
-	
+
 	/** @brief lee un fichero de delitos, linea a linea
 	@param[in] s nombre del fichero
         @param[in,out] C conjunto sobre el que se lee
@@ -18,7 +18,7 @@ bool load(conjunto &  C, const string & s) {
 
  cout << "Abrimos "<< s << endl;
  fe.open(s.c_str(), ifstream::in);
- if (fe.fail())    
+ if (fe.fail())
  {
    cerr << "Error al abrir el fichero " << s << endl;
  } else {
@@ -52,11 +52,11 @@ bool load(conjunto &  C, const string & s) {
 int main()
 {
     conjunto ChicagoDB;
-    
-    
+
+
     fecha f;
     long int n= 10222792;
-    
+
 
     load(ChicagoDB, "crimenes.csv");
     cout << ChicagoDB.empty() << endl;
@@ -79,7 +79,7 @@ int main()
     cout << *dit << endl << endl << endl << endl;
 
     cout << "b" << endl;
-    
+
     conjunto::const_iterator it(D.cend());
     --it;
     crimen a = (*D.cbegin()) ;
@@ -90,6 +90,14 @@ int main()
       --it;
     }
     cout << (it == D.cend()) << endl;
+
+		cout << "PROBANDO ADRESS ITERATOR" << endl;
+		conjunto::arrest_iterator ait;
+		ait = ChicagoDB.abegin();
+		cout << "Primer arrest: " << *ait << endl;
+
+		ait = ChicagoDB.aend();
+		cout << "Ultimo arrest: " << *ait << endl;
 
    return 0;
 }
